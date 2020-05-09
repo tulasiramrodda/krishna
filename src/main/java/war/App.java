968d988d17c41c9c7e1d021b96dfce1ff0ba2fb2
EC2 Default User <ec2-user@ip-172-31-29-79.ap-south-1.compute.internal>
2020-05-09 16:42:18 +0000

    1st commit

---
+++ src/main/java/war/App.java
@@ -0,0 +1,16 @@
+package war;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+	    System.out.println( "Hello World!" );
+	    System.out.println("Maven Project created by Krishna");
+
+    }
+}
